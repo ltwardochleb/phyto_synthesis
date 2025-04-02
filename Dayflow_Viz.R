d77439ae0233595552393521b44a906a2fe16b87
Berk <Alex.Berk@Waterboards.ca.gov>
2025-04-02 16:22:32 -0700

    Added some comments for clarity

--- Dayflow_Viz.R
+++ Dayflow_Viz.R
@@ -6,18 +6,27 @@ library(lubridate)
 #read in dayflow .csv
 
 dayflow <- read_csv("./Data/Dayflow/dayflow-results-1997-2023.csv")
+
+#Aggregate by month
 df_month <- dayflow %>% 
   group_by(Year,Month) %>% 
   summarise_if(is.numeric,mean) %>%
-  select(c("Year","Month","SAC","SJR"))
+  select(c("Year","Month","SAC","SJR")) #just looking at SAC and SJR for now
 
+#Put month and year in same column and 
+#(could probably do this faster as a mutate)
 df_month$Date <- paste(df_month$Year,df_month$Month,sep="-")
 df_month$Date <- ym(df_month$Date)
-help(format)
 
+#very rough plot of Sac and SJR
 ggplot() +
   geom_line(data=df_month,
             aes(x=Date,y=SAC),
             color="red") +
   geom_line(data=df_month,
             aes(Date,SJR))
+
+#more ideas - split out into critically dry, dry, wet, and average. There's likely some guidance on this somewhere. 
+#Aggregate by season?
+#How did Ellen define a wet/dry winter/spring? 
+
