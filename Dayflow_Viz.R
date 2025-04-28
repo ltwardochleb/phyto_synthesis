e1dd57febec9f14b79bfd8619dcbd2b379fb07e1
Berk <Alex.Berk@Waterboards.ca.gov>
2025-04-28 15:34:50 -0700

    fixed seasons, added percentiles and wet/dry-wet/dry indicators.

--- Dayflow_Viz.R
+++ Dayflow_Viz.R
@@ -24,17 +24,33 @@ df_month$water_year <- as.numeric(format(df_month$Date %m+% months(3),"%Y"))
 
 #create seasonal period column
 df_month$period <- character(length(nrow(df_month)))
-df_month$period[df_month$Month >= 10 | df_month$Month <= 3] <- "Oct-Mar"
-df_month$period[df_month$Month >= 4 & df_month$Month <= 9] <- "Apr-Sep" #can change or add periods based on study time frame
+df_month$period[df_month$Month >= 12 | df_month$Month <= 2] <- "Winter"
+df_month$period[df_month$Month >= 3 & df_month$Month <= 5] <- "Spring"
+df_month$period[df_month$Month >= 6 & df_month$Month <= 8] <- "Summer"
+df_month$period[df_month$Month >= 9 & df_month$Month <= 11] <- "Fall" #can change or add periods based on study time frame
 
 #pivot wider then summarize by year/season
-df_wide<- df_month %>% 
+df_wide<- df_month %>%
+  filter(!period == "Fall"& !period == "Winter") %>%
   pivot_wider(names_from=period,values_from=c(SAC,SJR)) %>%
   group_by(water_year) %>%
-  summarize(across(`SAC_Oct-Mar`:`SJR_Apr-Sep`,\(x) sum(x,na.rm=TRUE)))
+  summarize(across(`SAC_Spring`:`SJR_Summer`,\(x) mean(x,na.rm=TRUE)))%>%
+  mutate(across(`SAC_Spring`:`SJR_Summer`,list(perc=percent_rank)))
+
+df_wide$SAC_type <- character(length(nrow(df_month)))
+df_wide$SAC_type[df_wide$SAC_Spring_perc >= 0.5&df_wide$SAC_Summer_perc >= 0.5] <- "wet-to-wet"
+df_wide$SAC_type[df_wide$SAC_Spring_perc < 0.5&df_wide$SAC_Summer_perc >= 0.5] <- "dry-to-wet"
+df_wide$SAC_type[df_wide$SAC_Spring_perc >= 0.5&df_wide$SAC_Summer_perc < 0.5] <- "wet-to-dry"
+df_wide$SAC_type[df_wide$SAC_Spring_perc < 0.5&df_wide$SAC_Summer_perc < 0.5] <- "dry-to-dry"
+
+df_wide$SJR_type <- character(length(nrow(df_month)))
+df_wide$SJR_type[df_wide$SJR_Spring_perc >= 0.5&df_wide$SJR_Summer_perc >= 0.5] <- "wet-to-wet"
+df_wide$SJR_type[df_wide$SJR_Spring_perc < 0.5&df_wide$SJR_Summer_perc >= 0.5] <- "dry-to-wet"
+df_wide$SJR_type[df_wide$SJR_Spring_perc >= 0.5&df_wide$SJR_Summer_perc < 0.5] <- "wet-to-dry"
+df_wide$SJR_type[df_wide$SJR_Spring_perc < 0.5&df_wide$SJR_Summer_perc < 0.5] <- "dry-to-dry"
 
 #merge with Water year type based on runoff from DWR
-df_wide <- df_wide %>% merge(wy_type,by.x="water_year",by.y="WY")
+df_wide <- df_wide %>% merge(wy_type[c("WY","SAC Yr-type","SJR Yr-type")],by.x="water_year",by.y="WY")
   
 #very rough plot of Sac and SJR
 ggplot() +
@@ -44,12 +60,10 @@ ggplot() +
   geom_line(data=df_month,
             aes(Date,SJR))
 
-#bar chart by WY for Sac during non-irrigation season
-ggplot(df_wide,aes(water_year,`SAC_Oct-Mar`)) +
+#bar chart by WY for avg sac flow in spring, colored by WY type
+ggplot(df_wide,aes(water_year,`SAC_Spring`)) +
   geom_col(aes(fill=`SAC Yr-type`))
 
 
-#more ideas - split out into critically dry, dry, wet, and average. There's likely some guidance on this somewhere. 
-#Aggregate by season?
 #How did Ellen define a wet/dry spring/summer? 
 
