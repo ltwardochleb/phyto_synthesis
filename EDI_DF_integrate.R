ab0d164b712fe63c34a6316b3fbc45a0058157d8
Berk <Alex.Berk@Waterboards.ca.gov>
2025-06-09 16:32:41 -0700

    edi integration with dayflow

---
+++ EDI_DF_integrate.R
@@ -0,0 +1,26 @@
+#Dayflow and Discrete WQ Data integration - edi
+library(tidyverse)
+library(lubridate)
+
+#Dayflow -------------
+
+df1 <- read_csv("Data/Dayflow/dayflow-results-1970-1983.csv") %>%
+  select(c("Date","Year","Month","SAC","SJR","OUT")) #Sac, SJR, Delta Outflow
+
+df2 <- read_csv("Data/Dayflow/dayflow-results-1984-1996.csv") %>%
+  select(c("Date","Year","Month","SAC","SJR","OUT")) #Sac, SJR, Delta Outflow
+
+df3 <- read_csv("Data/Dayflow/dayflow-results-1997-2023.csv") %>%
+  select(c("Date","Year","Month","SAC","SJR","OUT")) #Sac, SJR, Delta Outflow
+
+dayflow <- rbind(df1,df2,df3)
+
+#Aggregate by month
+df_month <- dayflow %>% 
+  group_by(Year,Month) %>% 
+  summarise_if(is.numeric,mean) %>% 
+  mutate(year_month = parse_date(paste(Year,Month),"%Y %m"))
+
+#EDI ----------
+
+edi <- read.csv("Data/edi_clean.csv")
