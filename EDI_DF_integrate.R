6aaf0e919d38eae468db9779cb754369d07bdb06
Berk <Alex.Berk@Waterboards.ca.gov>
2025-06-11 16:42:00 -0700

    updated to change all < values to 0. Next step is to integrate DWR code (which is not straightforward and will not apply to whole dataframe)

--- EDI_DF_integrate.R
+++ EDI_DF_integrate.R
@@ -37,13 +37,71 @@ rm("dt2") #remove extra df
 colnames(dt1)
 
 #create year_month column and aggregate to monthly data.
-#QUESTION: what do I do with < values? filter them out? Convert to 0? 
+#QUESTION: what do I do with < values? filter them out? 
+
+#change < values to 0
+
+LOD <- function(data,colname_sign) {
+  index <- grep(colname_sign,colnames(data))
+  data[[index+1]][data[[index]] %in% "<"] <- 0 #or whatever factor we want to replace with
+  return(data)
+}
+
+colname_sign <- dt1 %>% select(ends_with("Sign")) %>% colnames
+
+for(n in 1:21) {
+  dt1 <- LOD(dt1,colname_sign[n])
+}
+
 dt1_mo <- dt1 %>% 
   mutate(Date = as.Date(as.character(Date))) %>%
   mutate(year_month = parse_date(paste(year(Date),month(Date)),"%Y %m")) %>%
   group_by(Source,Station,Latitude,Longitude,year_month) %>%
   summarise_if(is.numeric,mean)
-
+help(mutate)
 dt_df <- merge(dt1_mo,df_month,by="year_month",all.x=T)
+write.csv(dt_df,"Data/edi_df_integrate_monthly.csv")
 
-#QUESTION: how far back are we going? 
+#QUESTION: how far back are we going? 1979
+
+
+
+
+
+replace_blw_rl <- function(df, min_val = 0, seed = 1) {
+  # Pull out values that are below the RL
+  df_blw_rl <- df %>% filter(Sign == "<")
+  
+  # Replace below RL values with simulated ones
+  withr::with_seed(
+    # Set seed for reproducibility
+    seed = seed,
+    df_blw_rl_sim <- df_blw_rl %>% 
+      mutate(Result = round(runif(nrow(df_blw_rl), min = min_val, max = Result), 6))
+  )
+  
+  # Add simulated values back to main data frame
+  df %>% filter(Sign != "<") %>% bind_rows(df_blw_rl_sim)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
