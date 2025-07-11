3497c9ac0d3fef3afefbc6449f500ce8173cf058
Berk <Alex.Berk@Waterboards.ca.gov>
2025-07-11 09:34:54 -0700

    added region summary

--- bivariate_GAMS.R
+++ bivariate_GAMS.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(mgcv)
 library(MuMIn)
 library(gratia)
+library(sf)
 
 #Setting up GAMs for threshold
 
@@ -12,11 +13,31 @@ wq <- read.csv("Data/edi_df_integrate_monthly.csv")
 #1. Assign region to each station
 # do we already have a regional assignment somewhere?
 
+regions<-st_read("Regions_shp/Rosies_regions_edited.shp") %>%
+  filter(!is.na(Regions)) %>%
+  st_transform(26910)
+
+wq.sf <- wq %>%
+  filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  #convert to UTMs so it's in the same coordinate reference system as the Delta shapefile
+  st_transform(crs = 26910)
+
+wq <- st_join(wq.sf, regions["Regions"], join = st_intersects, left= TRUE) %>%  
+  filter(!is.na(Regions)) %>%
+  st_drop_geometry()
+
 #2. Calculate monthly average values for each region
+
+wq_r_sum <- wq %>% 
+  group_by(Regions,year_month) %>%
+  summarize_if(is.numeric,mean,na.rm=TRUE)
 
 #3. Assign a season to each month 
 wq$season <- replicate(nrow(wq),NA)
-wq$season[wq$Month %in% c(12,1,2)] <- "winter"
-wq$season[wq$Month %in% c(3,4,5)] <- "spring"
-wq$season[wq$Month %in% c(6,7,8)] <- "summer"
-wq$season[wq$Month %in% c(9,10,11)] <- "fall"
+wq$season[wq$Month %in% c(12,1,2)] <- "Winter"
+wq$season[wq$Month %in% c(3,4,5)] <- "Spring"
+wq$season[wq$Month %in% c(6,7,8)] <- "Summer"
+wq$season[wq$Month %in% c(9,10,11)] <- "Fall"
+
+
