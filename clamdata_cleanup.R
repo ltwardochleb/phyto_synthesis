6f17a6a478f0b4f52be6aa8943f56e87a7c952b0
Ryan Hankins <136652685+RyanAHankins@users.noreply.github.com>
2025-09-10 13:41:27 -0700

    added Index value

--- clamdata_cleanup.R
+++ clamdata_cleanup.R
@@ -1,9 +1,17 @@
 #Data wrangling and integrating clam  data
-#7/28/2025
+#9/9/2025
 
 #Read in packages
 library(readxl)
 library(tidyverse)
+library(lubridate)
+library(tidyverse)
+library(mgcv)
+library(sf)
+
+#############################
+# COMBINE THE CLAM DATASETS #
+#############################
 
 #read in EMP clam data and USGS clam data
 EMP_clam <- read.csv("Data/EMP biomass, GR, FR 2020-2023.csv")
@@ -11,7 +19,132 @@ USGS_clam <- read_excel("Data/Monthly Bivalve Metrics All - from USGS through 20
 
 #rename station names
 EMP_clam <- EMP_clam %>% mutate(station_name = str_extract(Station, "\\w+(?=[-]?)")) %>% mutate(station_name = paste0("EMP ", station_name))
+EMP_clam$Station <-gsub("-", "", EMP_clam$Station)
 USGS_clam <- USGS_clam %>% mutate(station_name = str_extract(Station, "\\w+(?=L|R|C)")) %>% mutate(station_name = ifelse(is.na(station_name), Station, station_name)) %>% mutate(station_name = paste0("EMP ", station_name))                   
 
 #changing USGS data to long format
 USGS_clam_2 <- USGS_clam %>% pivot_wider(names_from = Clam, values_from = c(Clam_Density_no_m2, Biomass_gAFDM_m2, Grazing_Rate_m3_m2_d, Filtration_Rate_m3_m2_d, Average_length_mm, Recruits_no_0.05m2))
+
+# add a date column to EMP_clam data
+EMP_clam_2 <- EMP_clam %>%
+  mutate(Month = match(Month, month.name))
+
+EMP_clam_2$Date <- make_date(EMP_clam_2$Year, EMP_clam_2$Month, 1)
+
+# USGS dataset is daily while EMP is monthly. Taking the monthly means of the USGS dataset before combining the two. 
+usgs_cols <- names(USGS_clam_2)
+usgs_cols_numeric <- usgs_cols[-1:-5]
+# Loop through each column name in the list
+for (col in usgs_cols_numeric) {
+  # Check if the column exists in the dataframe to avoid errors
+  if (col %in% names(USGS_clam_2)) {
+    USGS_clam_2[[col]] <- as.numeric(USGS_clam_2[[col]])}
+  }
+
+monthly_mean_usgs <- USGS_clam_2 %>%
+  mutate(Date = floor_date(Date, "month")) %>%
+  group_by(Date, station_name, Latitude, Longitude, Station) %>%
+  summarize(across(all_of(usgs_cols_numeric), ~mean(.x, na.rm = TRUE)), .groups = "drop")
+
+colnames(monthly_mean_usgs)
+colnames(EMP_clam_2)
+monthly_mean_usgs <- monthly_mean_usgs %>%
+  rename(
+    Corbicula_biomass = Biomass_gAFDM_m2_CF,
+    Potamocorbula_biomass = Biomass_gAFDM_m2_PA,
+    Corbicula_GR = Grazing_Rate_m3_m2_d_CF,
+    Potamocorbula_GR = Grazing_Rate_m3_m2_d_PA,
+    Corbicula_FR = Filtration_Rate_m3_m2_d_CF,
+    Potamocorbula_FR = Filtration_Rate_m3_m2_d_PA
+    
+  )
+
+usgs = subset(monthly_mean_usgs, select = c(Date, Station, station_name, Corbicula_biomass, Potamocorbula_biomass, Corbicula_GR, Potamocorbula_GR, Corbicula_FR, Potamocorbula_FR, Latitude, Longitude))
+emp = subset(EMP_clam_2, select = c(Date, Station, station_name, Corbicula_biomass, Potamocorbula_biomass, Corbicula_GR, Potamocorbula_GR, Corbicula_FR, Potamocorbula_FR, Latitude, Longitude))
+
+# combine the usgs and emp clam datasets
+combined_df <- rbind(usgs, emp)
+
+
+# write clam data to csv
+write.csv(combined_df, "Data/combined_clams_dataset.csv")
+
+#########################################################
+# COMBINE INTEGRATED DAY FLOW DATASET WITH CLAM DATASET #
+#########################################################
+
+# To combine with the day flow dataset, we need to assign regions to combined_df and then get average monthly values by region
+edi_integrate <- read_csv("Data/edi_df_integrate_monthly.csv")
+#read in region data
+regions <- st_read("Regions_shp/Rosies_regions_edited.shp")
+
+#change the crs to UTM zone 10N (EPSG 32610)
+regions<- st_transform(regions, crs =32610)
+
+#Attach Regions and Clean up Data
+
+edi_integrate.sf <- edi_integrate %>%
+  filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  #convert to UTMs so it's in the same coordinate reference system as the Delta shapefile
+st_transform(crs = 32610)
+
+edi_integrate <- st_join(edi_integrate.sf, regions["Regions"], join = st_intersects, left= TRUE) %>%  
+  filter(!is.na(Regions)) %>%
+  st_drop_geometry() 
+
+colnames(edi_integrate)
+#Calculate monthly average values for each region
+wq_r_sum <- edi_integrate %>% 
+  group_by(Regions,year_month) %>%
+  summarize_if(is.numeric,mean,na.rm=TRUE) %>%
+  mutate(year_month = ymd(year_month)) %>%
+  dplyr::select(year_month, Month, Regions, 
+           Chlorophyll,
+           DissAmmonia,
+           TotAmmonia,
+           Secchi,
+           TotPhos,
+           DissNitrateNitrite,
+           Temperature, 
+           TurbidityNTU,
+           Conductivity,
+           SAC,
+           Index)
+
+
+
+combined_df.sf <- combined_df %>%
+  filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  #convert to UTMs so it's in the same coordinate reference system as the Delta shapefile
+  st_transform(crs = 32610)
+
+combined_df <- st_join(combined_df.sf, regions["Regions"], join = st_intersects, left= TRUE) %>%  
+  filter(!is.na(Regions)) %>%
+  st_drop_geometry() 
+
+names(combined_df)[names(combined_df) == "Date"] <- "year_month"
+
+#Calculate monthly average values for each region
+combined_df_sum <- combined_df %>% 
+  group_by(Regions,year_month) %>%
+  summarize_if(is.numeric,mean,na.rm=TRUE) %>%
+  mutate(year_month = ymd(year_month))
+
+# Make the integrated dataset
+
+integrated_df <- full_join(combined_df_sum, wq_r_sum, by=c("Regions", "year_month"))
+
+
+# Assign seasons using ifelse statements
+integrated_df$season <- ifelse(integrated_df $Month %in% c(12, 1, 2), "Winter",
+                               ifelse(integrated_df $Month %in% c(3, 4, 5), "Spring",
+                                      ifelse(integrated_df$Month %in% c(6, 7, 8), "Summer",
+                                             "Autumn")))
+# Remove Suisun Marsh
+
+integrated_df <- integrated_df[integrated_df$Regions != "Suisun Marsh", ]
+
+# Write to csv
+write.csv(integrated_df, "Data/integrated_dataset.csv")                           
