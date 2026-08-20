29b49c6c4e60f0ea9b50d942802d2a5dc1548559
Schuyler Nardelli <schuylernardelli@gmail.com>
2026-08-20 16:23:39 -0700

    Added previously missing EMP and dayflow data.

--- load_USGSBay_data.R
+++ load_USGSBay_data.R
@@ -18,3 +18,23 @@ dbDisconnect(con)
 rm(con)
 
 save(cruiseall,nutrients, file = "Data/Peterson_data.Rdata")
+
+
+#--------------------------------------------------
+# Helper function for loading EDI entities
+#--------------------------------------------------
+read_edi_entity <- function(package_id, entity_name) {
+  entity_id <- read_data_entity_names(package_id) %>%
+    filter(entityName == entity_name) %>%
+    pull(entityId)
+  raw_file <- read_data_entity(package_id, entity_id)
+  readr::read_csv(raw_file, show_col_types = FALSE)
+}
+
+#--------------------------------------------------
+# Load PESP data
+#--------------------------------------------------
+
+pesp <- read_edi_entity(package_id = "edi.2209.3",entity_name = "PESP_enumeration.csv") 
+emp <- read_edi_entity(package_id = "edi.458.14",entity_name = "EMP_DWQ_1975-2024") 
+
